b1d4718561ffac09d10fa2e2ebe384e307e3e564
tjswodud <tjswodud85@gmail.com>
2022-04-09 15:46:48 +0900

    [LAB04] lab04 submitted

--- Source/Game/Cube/CustomCube.cpp
+++ Source/Game/Cube/CustomCube.cpp
@@ -2,6 +2,15 @@
 
 void CustomCube::Update(_In_ FLOAT deltaTime)
 {
+	/*
+	XMVECTOR translateVector = { 4.0f, 0.0f, 0.0f };
+
+	RotateY(-deltaTime);
+	RotateZ(-deltaTime * 5.0f);
+	Translate(translateVector);
+	Scale(0.5f, 0.5f, 0.3f);
+	*/
+
 	XMMATRIX mSpin = XMMatrixRotationY(-deltaTime);
 	XMMATRIX mOrbit = XMMatrixRotationZ(-deltaTime * 5.0f);
 	XMMATRIX mTranslate = XMMatrixTranslation(4.0f, 0.0f, 0.0f);
--- Source/Library/Renderer/Renderable.cpp
+++ Source/Library/Renderer/Renderable.cpp
@@ -12,8 +12,8 @@ namespace library
                 ID3D11DeviceContext* pImmediateContext
                   The Direct3D context to set buffers
 
-      Modifies: [m_vertexBuffer, m_indexBuffer, m_constantBuffer, 
-                  m_world].
+      Modifies: [m_vertexBuffer, m_indexBuffer, m_constantBuffer,
+                 m_world].
 
       Returns:  HRESULT
                   Status code
@@ -80,7 +80,7 @@ namespace library
     /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
       Method:   Renderable::SetVertexShader
 
-      Summary:  Sets the vertex shader to be used for this renderable 
+      Summary:  Sets the vertex shader to be used for this renderable
                 object
 
       Args:     const std::shared_ptr<VertexShader>& vertexShader
@@ -198,5 +198,91 @@ namespace library
     const XMMATRIX& Renderable::GetWorldMatrix() const
     {
         return m_world;
+    }
+
+    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+      Method:   Renderable::RotateX
+      Summary:  Rotates around the x-axis
+      Args:     FLOAT angle
+                  Angle of rotation around the x-axis, in radians
+      Modifies: [m_world].
+    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+    void Renderable::RotateX(_In_ FLOAT angle)
+    {
+        // m_world *= x-axis rotation by angle matrix
+        m_world *= XMMatrixRotationX(angle);
+    }
+
+    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+      Method:   Renderable::RotateY
+      Summary:  Rotates around the y-axis
+      Args:     FLOAT angle
+                  Angle of rotation around the y-axis, in radians
+      Modifies: [m_world].
+    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+    void Renderable::RotateY(_In_ FLOAT angle)
+    {
+        // m_world *= y-axis rotation by angle matrix
+        m_world *= XMMatrixRotationY(angle);
+    }
+
+    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+      Method:   Renderable::RotateZ
+      Summary:  Rotates around the z-axis
+      Args:     FLOAT angle
+                  Angle of rotation around the z-axis, in radians
+      Modifies: [m_world].
+    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+    void Renderable::RotateZ(_In_ FLOAT angle)
+    {
+        // m_world *= z-axis rotation by angle matrix
+        m_world *= XMMatrixRotationZ(angle);
+    }
+
+    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+      Method:   Renderable::RotateRollPitchYaw
+      Summary:  Rotates based on a given pitch, yaw, and roll (Euler angles)
+      Args:     FLOAT pitch
+                  Angle of rotation around the x-axis, in radians
+                FLOAT yaw
+                  Angle of rotation around the y-axis, in radians
+                FLOAT roll
+                  Angle of rotation around the z-axis, in radians
+      Modifies: [m_world].
+    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+    void Renderable::RotateRollPitchYaw(_In_ FLOAT pitch, _In_ FLOAT yaw, _In_ FLOAT roll)
+    {
+        // m_world *= x, y, z-axis rotation by pitch, yaw, roll matrix
+        m_world *= XMMatrixRotationRollPitchYaw(pitch, yaw, roll);
+    }
+
+    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+      Method:   Renderable::Scale
+      Summary:  Scales along the x-axis, y-axis, and z-axis
+      Args:     FLOAT scaleX
+                  Scaling factor along the x-axis.
+                FLOAT scaleY
+                  Scaling factor along the y-axis.
+                FLOAT scaleZ
+                  Scaling factor along the z-axis.
+      Modifies: [m_world].
+    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+    void Renderable::Scale(_In_ FLOAT scaleX, _In_ FLOAT scaleY, _In_ FLOAT scaleZ)
+    {
+        // m_world *= x, y, z-axis scaling by scale factor matrix
+        m_world *= XMMatrixScaling(scaleX, scaleY, scaleZ);
+    }
+
+    /*M+M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M+++M
+      Method:   Renderable::Translate
+      Summary:  Translates matrix from a vector
+      Args:     const XMVECTOR& offset
+                  3D vector describing the translations along the x-axis, y-axis, and z-axis
+      Modifies: [m_world].
+    M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M---M-M*/
+    void Renderable::Translate(_In_ const XMVECTOR& offset)
+    {
+        // m_world *= translate by offset vector matrix
+        m_world *= XMMatrixTranslationFromVector(offset);
     }
 }
