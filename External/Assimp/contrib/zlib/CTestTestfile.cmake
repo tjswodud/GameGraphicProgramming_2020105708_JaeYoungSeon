9d814cb9bc0101f77ec53fb6c2546a40e9670239
tjswodud <tjswodud85@gmail.com>
2022-05-05 20:01:12 +0900

    [LAB07] lab07 submitted (deleted assimp setting files)

--- External/Assimp/contrib/zlib/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/tjswo/assimp-master/contrib/zlib
-# Build directory: C:/Users/tjswo/assimp-master/contrib/zlib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
